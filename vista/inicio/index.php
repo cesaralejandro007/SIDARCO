526c3478f703f5df8a54348fe11db6abf20e843a
Alex37 <cesaralejandrovides2023@gmail.com>
2022-12-27 15:52:29 -0400

    cambio de colores

--- vista/inicio/index.php
+++ vista/inicio/index.php
@@ -25,7 +25,7 @@
         <div class="row">
             <div class="col-12 col-sm-6 col-md-4">
                 <div class="info-box">
-                    <span class="info-box-icon elevation-1" style="background: #00535E;color: white;">
+                    <span class="info-box-icon elevation-1" style="background: #15406D;color: white;">
                         <i class="fa fa-users"></i>
                     </span>
 
@@ -52,11 +52,11 @@
             <!-- /.col -->
             <div class="col-12 col-sm-6 col-md-4">
                 <div class="info-box mb-3">
-                    <span class="info-box-icon elevation-1" style="background: #00535E;color: white;"><i class="mdi mdi-home-outline"></i></span>
+                    <span class="info-box-icon elevation-1" style="background: #15406D;color: white;"><i class="mdi mdi-home-outline"></i></span>
 
                     <div class="info-box-content">
                         <a href="javascript:void(0)">
-                            <span class="info-box-text">
+                            <span style="background:#c70039">
                               <!--
                                     <h3>Viviendas</h3> -->
                             </span>
@@ -82,12 +82,12 @@
 
             <div class="col-12 col-sm-6 col-md-4">
                 <div class="info-box mb-3">
-                    <span class="info-box-icon  elevation-1" style="background: #00535E;color: white;"><i class="mdi mdi-view-parallel"
+                    <span class="info-box-icon  elevation-1" style="background: #15406D;color: white;"><i class="mdi mdi-view-parallel"
                             style="color: white;"></i></span>
 
                     <div class="info-box-content">
                         <a href="javascript:void(0)">
-                            <span class="" >
+                            <span class=""  >
                              <!--   <h3>Calles</h3>-->
                             </span>
                         </a>
@@ -163,7 +163,7 @@
   
             <div class="col-md-4">
                 <!-- Info Boxes Style 2 -->
-                <div class="info-box mb-3 " style="background: #02A2A1;color: white;">
+                <div class="info-box mb-3 " style="background:#c70039;color: white;">
                     <span class="info-box-icon"><i class="fa fa-users"></i></span>
 
                     <div class="info-box-content">
@@ -173,7 +173,7 @@
                     <!-- /.info-box-content -->
                 </div>
                 <!-- /.info-box -->
-                <div class="info-box mb-3 " style="background: #02A2A1;color: white;">
+                <div class="info-box mb-3 " style="background: #c70039;color: white;">
                     <span class="info-box-icon"><i class="fa fa-user-o"></i></span>
 
                     <div class="info-box-content">
@@ -183,7 +183,7 @@
                     <!-- /.info-box-content -->
                 </div>
                 <!-- /.info-box -->
-                <div class="info-box mb-3 " style="background: #02A2A1;color: white;">
+                <div class="info-box mb-3 " style="background: #c70039;color: white;">
                     <span class="info-box-icon"><i class="fa fa-child"></i></span>
 
                     <div class="info-box-content">
@@ -193,7 +193,7 @@
                     <!-- /.info-box-content -->
                 </div>
                 <!-- /.info-box -->
-                <div class="info-box mb-3 " style="background: #02A2A1;color: white;">
+                <div class="info-box mb-3 " style="background: #c70039;color: white;">
                     <span class="info-box-icon"><i class="fa fa-address-card-o"></i></span>
 
                     <div class="info-box-content">
