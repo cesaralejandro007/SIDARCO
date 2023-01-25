d81976fa5cbb2d295040412c13c207ba07c9b020
marijozapatav <MZAPATA@seniat.gov.ve>
2023-01-25 08:37:13 -0400

    cambio en registrar persona

--- vista/personas/registrar.php
+++ vista/personas/registrar.php
@@ -30,8 +30,8 @@
                         <div class="col-md-12">
                             <div class="vtabs">
                                 <ul class="nav nav-tabs tabs-vertical" role="tablist">
-                                    <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;" >
-                                        <a class="nav-link active" id='tab_1' style="font-weight:bold">
+                                    <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
+                                        <a class="nav-link active" id='tab_1'>
                                             <span class="hidden-sm-up">
                                                 <i class="ti-home"></i>
                                             </span>
@@ -42,7 +42,7 @@
                                     </li>
                                     
                                     <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
-                                        <a id='tab_2' style="font-weight:bold">
+                                        <a id='tab_2' >
                                             <span class="hidden-sm-up">
                                                 <i class="ti-user"></i>
                                             </span>
@@ -52,13 +52,8 @@
                                             </span>
                                         </a>
                                     </li>
-<<<<<<< HEAD
-                                         
-=======
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
-
                                     <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
-                                        <a id='tab_3' style="font-weight:bold">
+                                        <a id='tab_3' >
                                             <span class="hidden-sm-up">
                                                 <i class="ti-email"></i>
                                             </span>
@@ -68,16 +63,10 @@
                                             </span>
                                         </a>
                                     </li>
-<<<<<<< HEAD
 
                                     <!--
                                     <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
                                         <a id='tab_3' >
-=======
-                                    
-                                    <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
-                                        <a id='tab_4' style="font-weight:bold">
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
                                             <span class="hidden-sm-up">
                                                 <i class="ti-email"></i>
                                             </span>
@@ -88,7 +77,7 @@
                                     </li>
 --><!--
                                     <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
-                                        <a id='tab_5' style="font-weight:bold">
+                                        <a id='tab_5'>
                                             <span class="hidden-sm-up">
                                                 <i class="ti-email"></i>
                                             </span>
@@ -99,11 +88,7 @@
                                     </li>
 -->
                                     <li class="nav-item" style="margin-top: 50%; margin-bottom: 50%;">
-<<<<<<< HEAD
                                         <a id='tab_4'>
-=======
-                                        <a id='tab_6' style="font-weight:bold">
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
                                             <span class="hidden-sm-up">
                                                 <i class="ti-email"></i>
                                             </span>
@@ -263,25 +248,6 @@
 
 
                                         <div class="col-md-6 mt-2">
-                                <label for="tiempo_comunidad">
-                                    Tiempo en la comunidad    
-                                </label>
-                                <span style='display:none;color:red' id='valid_13'>Campo vacío</span>
-                                <div class="input-group">
-                                    <input  class="form-control mb-10" id="tiempo_comunidad"
-                                    name="datos[tiempo_comunidad]"
-                                    type="date" />
-                                    <button type="button" class="btn btn-default" id='desde_siempre'>Desde siempre</button>
-                                </div>
-
-
-                            </div>
-
-
-
-
-
-                                        <div class="col-md-6 mt-2">
                                             <label for="sexualidad">
                                                 Orientación sexual
                                             </label>
@@ -309,11 +275,7 @@
                                     </div>
 
 
-<<<<<<< HEAD
 -->
-=======
-
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
 
                                     <div class="col-md-6 mt-2">
                                         <label for="nacionalidad">
@@ -365,12 +327,7 @@
                              </div>
 -->
 
-<<<<<<< HEAD
 <!--
-=======
-
-
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
 
                             <div class="col-md-4 mt-2">
                                 <label for="jefe_familia">
@@ -391,13 +348,8 @@
 
                          </select>
                      </div>
-<<<<<<< HEAD
 -->
 <!--
-=======
-
-
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
                      <div class="col-md-4 mt-2">
                         <label for="propietario_vivienda">
                             Propietario de vivienda
@@ -419,12 +371,7 @@
              </div>
 -->
 
-<<<<<<< HEAD
 <!--
-=======
-
-
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
              <div class="col-md-4 mt-2">
                 <label for="jefe_calle">
                     Jefe de calle
@@ -535,13 +482,8 @@
 </td></tr>
 </table>
 </div>
-<<<<<<< HEAD
            -->
 <!--
-=======
-          
-
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
 <div class="col-md-6 mt-2">
     <label for="privado_libertad">
         Privado de libertad
@@ -561,11 +503,7 @@
 
 </select>
 </div>
-<<<<<<< HEAD
            -->
-=======
-           
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
 <!-- 
                                             <div class="col-md-6 mt-2">
                                                 <label for="institucion">
@@ -584,13 +522,8 @@
                                         </div>
 
                                     </div>
-<<<<<<< HEAD
                                      <!--
                                     <div class="tab-pane" id="panel3" role="tabpanel">
-=======
-                          
-                                    <div class="tab-pane" id="panel2" role="tabpanel">
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
                                         <div class="row">
                                             <div class="col-md-12 text-center">
                                                 <h2>
@@ -677,13 +610,11 @@
                                         </div>
 
                                     </div>
-<<<<<<< HEAD
            -->
                                     <div class="tab-pane" id="panel2" role="tabpanel">
-=======
 
                                     <div class="tab-pane" id="panel3" role="tabpanel">
->>>>>>> 5aecd3e7247cd2f43bb226c02f0b5a6a6143adb3
+
                                         <div class="row">
                                             <div class="col-md-12 text-center">
                                                 <h2>
@@ -1131,14 +1062,14 @@
 
     <div class="col-md-12 mt-4">
         <div style="float: left;">
-            <a id="anterior" style='display:none' type="button" class="btn"style="background:#15406D; color:white">
+            <a id="anterior" style='display:none' type="button" class="btn  btn-info">
                 Anterior
             </a>
         </div>
     </div>
     <div class="col-md-12 mt-4">
         <div style="float: right;">
-            <a id="siguiente" type="button" class="btn"style="background:#15406D; color:white">
+            <a id="siguiente" type="button" class="btn  btn-info">
                 Siguiente
             </a>
         </div>
